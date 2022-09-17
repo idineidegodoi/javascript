f643ab9dbc819aac98724c92b5b1d6bbce0a3bd7
Idinei de Godoi <idineig@gmail.com>
2022-09-17 17:13:46 -0300

    Exercício de funções e operadores de atribuição no JavaScript

---
+++ funcoes/script.js
@@ -0,0 +1,29 @@
+//funções
+function escreverTexto(){ //função que não passa parâmetros.
+    //função que escreve um texto direto na tela através de um alerta.
+    let texto = 'Estou escrevendo texto em JavaScript!!!';
+    return texto;
+} 
+
+//função com parâmtros
+/*As funções de operador de atribuição conjugadas com operadores lógicos(soma, subtração, etc..
+    evitam linhas de código, deixando o código mais enxuto 
+    e eficiente)*/
+
+function operadorSoma(n1, n2){/*função de operador que atribui e soma.
+sem a necessidade de declarar a variável dentro da função*/
+    n1 += n2;
+    return n1;
+
+}
+
+function operadorSubtracao(num1, num2){//função com operador que atribui e subtrai.
+    num1 -= num2;
+    return num1;
+
+}
+
+function operadorMultiplica(mult1, mult2){
+    mult1 *= mult2;
+    return mult1;
+}
