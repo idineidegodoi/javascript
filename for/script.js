41f77dff6392dd0ca95d866a2e4b4273a3dc05ce
Idinei de Godoi <idineig@gmail.com>
2022-09-19 17:56:01 -0300

    implementação do exercício do FOR

---
+++ for/script.js
@@ -0,0 +1,14 @@
+// função para executar uma rotina utilizando o FOR.
+
+function numeros(i, n1){
+
+    for(i; i <= 10; i++){
+
+        document.write(`O valor númérico é: ${i} <br>`);
+
+        n1 = n1 + 2;
+        document.write(`O valor da variável total: ${n1} <br><hr>`)
+
+    }
+
+}
